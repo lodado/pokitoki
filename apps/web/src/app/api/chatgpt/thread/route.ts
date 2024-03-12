ef1aa3b348ec7620b645eea8fbde4103546b861d
sap03110 <wastfg6972@naver.com>
2024-03-12 17:58:20 +0900

    feat: thread 기준 next api route 작성

---
+++ apps/web/src/app/api/chatgpt/thread/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { getLoginSession } from '@/hooks/login'
+import ChatGptService from '@/server/service/chatgpt/ChatGptService'
+
+const { getChats, createChat } = ChatGptService
+
+export const GET = async (req: NextRequest) => {
+  const assistantId = req.nextUrl.searchParams.get('assistantId')
+  if (!assistantId) return Response.json(false)
+
+  const { user } = await getLoginSession()
+  const userId = user.id
+
+  const threadIds = await getChats(userId, assistantId)
+  return NextResponse.json(threadIds)
+}
+
+export const POST = async (req: NextRequest) => {
+  const { assistantId } = await req.json()
+  if (!assistantId) return Response.json(false)
+
+  const { user } = await getLoginSession()
+  const userId = user.id
+
+  const threadId = await createChat(userId, assistantId)
+  return NextResponse.json(!!threadId)
+}
