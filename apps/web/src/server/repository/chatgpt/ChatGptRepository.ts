c51b478ed042812f08033c8c6e356070a8d5f7e3
sap03110 <wastfg6972@naver.com>
2024-03-12 18:22:07 +0900

    refactor: snake -> camel, 테이블명 변경

--- apps/web/src/server/repository/chatgpt/ChatGptRepository.ts
+++ apps/web/src/server/repository/chatgpt/ChatGptRepository.ts
@@ -1,27 +1,25 @@
 import { supabaseInstance } from '@/lib/supabase'
 
-// 사용자별 해당 튜터의 스레드(채팅방) ID를 가져옴
+// 사용자별 해당 튜터의 스레드(채팅방) 정보를 가져옴
 export const getThreadIds = async (userId: string, assistantId: string) => {
   const { data: threadIds } = await supabaseInstance
-    .from('user_threads')
-    .select('thread_id')
-    .eq('user_id', userId)
-    .eq('assistant_id', assistantId)
+    .from('threads')
+    .select('threadId, threadName')
+    .eq('userId', userId)
+    .eq('assistantId', assistantId)
   return threadIds
 }
 
-// 사용자별 해당 튜터의 스레드(채팅방) ID를 저장
+// 사용자별 해당 튜터의 스레드(채팅방) 정보를 저장
 export const insertThread = async (userId: string, assistantId: string, threadId: string) => {
-  const { error } = await supabaseInstance
-    .from('user_threads')
-    .insert({ user_id: userId, assistant_id: assistantId, thread_id: threadId } as never)
+  const { error } = await supabaseInstance.from('threads').insert({ userId, assistantId, threadId } as never)
 
   return { isSuccess: !error, error }
 }
 
-// 사용자별 해당 튜터의 스레드(채팅방) ID를 제거
+// 사용자별 해당 튜터의 스레드(채팅방) 정보를 제거
 export const deleteThread = async (userId: string, threadId: string) => {
-  const { error } = await supabaseInstance.from('user_threads').delete().eq('user_id', userId).eq('thread_id', threadId)
+  const { error } = await supabaseInstance.from('user_threads').delete().eq('userId', userId).eq('threadId', threadId)
 
   return { isSuccess: !error, error }
 }
