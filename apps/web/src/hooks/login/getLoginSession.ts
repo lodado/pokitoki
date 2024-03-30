263bbcf94953502843f9c093b1d6f0c7b688acd6
lodado <ycp998@naver.com>
2024-03-31 00:51:55 +0900

    chore: session 항상 불러오진 않게 변경

--- apps/web/src/hooks/login/getLoginSession.ts
+++ apps/web/src/hooks/login/getLoginSession.ts
@@ -3,14 +3,17 @@ import { NextAuthSessionResponse } from '@/server/service/auth/type'
 import { auth } from '../../lib/nextAuth/auth'
 
 const getLoginSession = async () => {
-  const session = await auth()
+  let session
 
-  if (session?.user)
+  if (!session) {
+    session = await auth()
+
     session.user = {
       name: session.user.name,
       email: session.user.email,
       picture: session.user.picture,
     } // filter out sensitive data
+  }
 
   return session as NextAuthSessionResponse
 }
