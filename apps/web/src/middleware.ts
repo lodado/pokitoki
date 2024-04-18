5f9eb9b7bc7f95f5a23376ffd228ad9a3ca90b73
chungheon_yi <chungheon_yi@tmax.co.kr>
2024-04-18 23:34:04 +0900

    fix: login 화면에서도 i18n 적용

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -6,26 +6,6 @@ import { auth, authConfig, NextAuth } from './lib/nextAuth'
 
 export const runtime = 'nodejs'
 
-const withAuthMiddleware = async (request: NextRequest, path: string, defaultLocale: string) => {
-  const session = await auth()
-  const response = NextResponse.next()
-
-  // protected
-  if (!session && (path.startsWith('/protected') || path.startsWith('/api/protected'))) {
-    if (!session) return NextResponse.json({ message: 'Login required.' }, { status: 401 })
-  }
-
-  // after login
-  if (session && path.endsWith('/login')) {
-    const url = request.nextUrl.clone()
-    url.pathname = `${defaultLocale}/protected/dashboard`
-
-    return NextResponse.redirect(url)
-  }
-
-  return response
-}
-
 const i18nMiddleware = async (request: NextRequest, path: string, defaultLocale: string) => {
   const handleI18nRouting = createIntlMiddleware(i18nOption as any)
   const response = handleI18nRouting(request)
@@ -35,11 +15,35 @@ const i18nMiddleware = async (request: NextRequest, path: string, defaultLocale:
   return response
 }
 
+const withAuthMiddleware = async (request: NextRequest, path: string, defaultLocale: string) => {
+  const session = await auth()
+  const response = NextResponse.next()
+
+  // private page
+  if (!session && (path.startsWith('/protected') || path.startsWith('/api/protected'))) {
+    if (!session) return NextResponse.json({ message: 'Login required.' }, { status: 401 })
+  }
+
+  if (path.endsWith('/login')) {
+    // after login
+    if (session) {
+      const url = request.nextUrl.clone()
+      url.pathname = '/protected/dashboard'
+      return NextResponse.redirect(url)
+    }
+
+    // before login
+    return i18nMiddleware(request, path, defaultLocale)
+  }
+
+  return response
+}
+
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname
   const defaultLocale = request.headers.get('x-your-custom-locale') || 'en'
 
-  if (path.endsWith('/api') || path.endsWith('/login')) {
+  if (path.startsWith('/api') || path.endsWith('/login')) {
     return withAuthMiddleware(request, path, defaultLocale)
   }
 
