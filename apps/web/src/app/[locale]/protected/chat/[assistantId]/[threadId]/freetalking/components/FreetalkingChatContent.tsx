d2c3b8e7835cacfd8c0aff1cd770f34711d8b9de
chungheon_yi <chungheon_yi@tmax.co.kr>
2024-05-04 23:50:16 +0900

    refactor: suspense 삭제 - 아직 next14에서 안되는듯 ㅡㅡ

--- apps/web/src/app/[locale]/protected/chat/[assistantId]/[threadId]/freetalking/components/FreetalkingChatContent.tsx
+++ apps/web/src/app/[locale]/protected/chat/[assistantId]/[threadId]/freetalking/components/FreetalkingChatContent.tsx
@@ -1,27 +1,14 @@
 'use client'
 
-import { ErrorSuspense, RenderFallbackProps } from '@custompackages/designsystem'
 import React, { Suspense } from 'react'
 
 import { ChatContent } from '../../../../components/ChatContent'
 import { useChatContentQuery } from '../../../../components/ChatContent/hooks'
 
-const RawFreetalkingChatContent = () => {
+const FreetalkingChatContent = () => {
   const { data: messages } = useChatContentQuery({ isInitFetchAllowed: false })
 
-  return (
-    <>
-      <ChatContent messages={messages} />
-    </>
-  )
-}
-
-const FreetalkingChatContent = () => {
-  return (
-    <Suspense key="abc">
-      <RawFreetalkingChatContent />
-    </Suspense>
-  )
+  return <ChatContent messages={messages} />
 }
 
 export default FreetalkingChatContent
