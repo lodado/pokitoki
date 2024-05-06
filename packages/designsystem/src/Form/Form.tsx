bf729c062f9ae9e8546843f650d7febc9ce47648
lodado <ycp998@naver.com>
2024-05-06 18:19:59 +0900

    feat: form 기본 component 구현

---
+++ packages/designsystem/src/Form/Form.tsx
@@ -0,0 +1,39 @@
+import React, { ComponentProps, PropsWithChildren } from 'react'
+
+import { Control, Field, Label, Message, Root, Submit, ValidityState } from './components/radix'
+
+const Form = ({ children }: PropsWithChildren) => {
+  return <Root>{children}</Root>
+}
+
+const RawField = (props: ComponentProps<typeof Field>) => {
+  return <Field {...props} />
+}
+
+const RawLabel = (props: ComponentProps<typeof Label>) => {
+  return <Label {...props} />
+}
+
+const RawControl = (props: ComponentProps<typeof Control>) => {
+  return <Control asChild {...props} />
+}
+
+const RawMessage = (props: ComponentProps<typeof Message>) => {
+  return <Message {...props} />
+}
+
+const RawSubmit = (props: ComponentProps<typeof Submit>) => {
+  return <Submit asChild {...props} />
+}
+
+Form.Field = RawField
+
+Form.Label = RawLabel
+
+Form.Message = RawMessage
+
+Form.Submit = RawSubmit
+
+Form.Control = RawControl
+
+export default Form
---
+++ packages/designsystem/src/Form/components/radix.tsx
@@ -0,0 +1,1 @@
+export { Control, Field, Label, Message, Root, Submit, ValidityState } from '@radix-ui/react-form'
